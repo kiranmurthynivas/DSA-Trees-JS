56cd02d1c34acacbbea5f2ef8db84aab2f6fb646
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-07-30 19:06:11 +0530

    Traversals In-pre-post DFS

--- tree_practice.js
+++ tree_practice.js
@@ -6,10 +6,7 @@ class TreeNode {
     }
 }
 
-
 const root = new TreeNode(10);
-
-
 
 root.left = new TreeNode(5);
 root.right = new TreeNode(15);
@@ -20,5 +17,22 @@ root.left.right = new TreeNode(7);
 
 console.log("--- Tree Proof Check ---");
 console.log("Root:", root.value); 
-console.log("Right Child:", root.right?.value); 
+console.log("Right Child:", root.right?.value); // ? used for optimal searching to prevent crashes
 console.log("Left-Right Grandchild:", root.left?.right?.value); 
+
+
+function Traversal(node) {
+
+    if( node === null ) { 
+        return;
+    }
+    
+    //recursion
+    Traversal(node.left);
+    Traversal(node.right);
+    console.log(node.value);
+}
+
+console.log(" In order Traversal");
+Traversal(root);
+
